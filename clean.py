76563a3708857208f2712944a28172bcd90d9c78
Mathieu Boucher <mathieu.boucher@epsi.fr>
2022-02-10 17:02:45 +0100

    Add drop_duplicates(Ã), determine gender of firstnames, drop lines with firstname or surname < len(3Ã)

--- clean.py
+++ clean.py
@@ -2,35 +2,47 @@ import pandas as pd
 import sys
 import json
 import numpy as np
+from progress.bar import Bar
 
 from urllib.request import urlopen
 
-firstnames_reader = pd.read_csv('./prenoms.csv', 'r', delimiter=";", usecols=['prenom', 'genre'], encoding='latin1')
+# df1 = liste de tous les prénoms
+df1 = pd.read_csv('./prenoms.csv', 'r', delimiter=";", usecols=['prenom', 'genre'], encoding='latin1')
 
 path = sys.argv[1]
 
-extract_file_reader = pd.read_csv(path, "r",
-                                  delimiter=";",
-                                  usecols=['prenom', 'genre'],
-                                  encoding='latin1')
+# df2 = liste kiute à compléter
+df2 = pd.read_csv(path, "r",
+                  delimiter=";",
+                  usecols=['Nom', 'prenom', 'genre'],
+                  encoding='latin1')
 
-#extract_file_reader.drop_duplicates(subset=['Nom', 'prenom'], keep='first').reset_index(drop=True)
-new_file = pd.DataFrame(extract_file_reader,
-                        columns=['Prénom', 'Genre'])
+df2 = df2.drop_duplicates(subset=['Nom', 'prenom'], keep='first', inplace=False).reset_index(drop=True)
 
-# for name1 in extract_file_reader['Prénom'].str.lower():
-#     for name2 in firstnames_reader['prenom']:
-#         if name1 == name2:
-#             print("   ")
+# Mettre en minuscules toutes les colonnes
+df1['prenom'] = df1['prenom'].str.capitalize()
 
+# création d'une nouvelle dataframe comprenant uniquement les lignes avec les cellules 'genre' manquantes
+missing = df2.loc[df2['genre'].isna()]
 
-extract_file_reader['Genre déterminé'] = np.where(extract_file_reader['prenom'] == firstnames_reader['prenom'], True, False)
+# Suppression des lignes dont les prénoms ou les noms sont inférieurs à 3 caractères
+indexNames = df2[len(df2['prenom']) < 3 or len(df2['nom'])].index
+df2.drop(indexNames, inplace=True)
 
-#if pd.isna(extract_file_reader['Genre']):
+bar = Bar('Processing', max=len(df2.index))
+for index2 in df2.index:
+    p2 = df2['prenom'][index2]
+    g2 = df2['genre'][index2]
+    for index in df1.index:
+        p = df1['prenom'][index]
+        g = df1['genre'][index]
+        if np.array_equiv(p, p2):
+            df2.at[index2, 'genre'] = g
+            print('prenom : ', p2, ' - genre : ', g)
+            bar.next()
+            break
+bar.finish()
 
-print(extract_file_reader)
-print(firstnames_reader)
+new_file = pd.DataFrame(df2, columns=['Nom', 'prenom', 'genre'])
 
-export_csv = new_file.to_csv('resultat.csv', index=False, header=True, encoding='utf-8', sep=';')
-
-print(extract_file_reader.reset_index(drop=True).equals(firstnames_reader.reset_index(drop=True)))
+export_csv = new_file.to_csv('resultat.csv', index=False, header=True, encoding='latin1', sep=';')
